bf589f50c4eb722a4d8ccbfc9b778951dbcbbd7a
shicai.xu <chenglingsu@126.com>
2023-04-06 14:25:32 +0800

    fix CheckParallelizer for multi tables

--- ape-dts/src/pipeline/check_parallelizer.rs
+++ ape-dts/src/pipeline/check_parallelizer.rs
@@ -34,36 +34,21 @@ impl Parallelizer for CheckParallelizer {
         data: Vec<RowData>,
         sinkers: &Vec<Arc<async_mutex::Mutex<Box<dyn Sinker + Send>>>>,
     ) -> Result<(), Error> {
-        let (batch_sub_datas, serial_sub_datas) = self.partition(data).await?;
-        ParallelizerUtil::sink(batch_sub_datas, sinkers, self.parallel_size, true)
-            .await
-            .unwrap();
-        ParallelizerUtil::sink(serial_sub_datas, sinkers, self.parallel_size, false)
-            .await
-            .unwrap();
-        Ok(())
-    }
-}
-
-impl CheckParallelizer {
-    async fn partition(
-        &mut self,
-        data: Vec<RowData>,
-    ) -> Result<(Vec<Vec<RowData>>, Vec<Vec<RowData>>), Error> {
-        let full_tb = format!("{}.{}", data[0].db, data[0].tb);
-        let mut batch_sub_datas = Vec::new();
-        let mut serial_sub_datas = Vec::new();
-
-        // data are all from the same table
         let mut merged_datas = self.merger.merge(data).await?;
-        if let Some(tb_merged_data) = merged_datas.get_mut(&full_tb) {
+        for (_full_tb, tb_merged_data) in merged_datas.iter_mut() {
             let batch_data = tb_merged_data.get_insert_rows();
-            batch_sub_datas = SnapshotParallelizer::partition(batch_data, self.parallel_size)?;
+            let batch_sub_datas = SnapshotParallelizer::partition(batch_data, self.parallel_size)?;
+            ParallelizerUtil::sink(batch_sub_datas, sinkers, self.parallel_size, true)
+                .await
+                .unwrap();
 
             let serial_data = tb_merged_data.get_unmerged_rows();
-            serial_sub_datas = SnapshotParallelizer::partition(serial_data, self.parallel_size)?;
+            let serial_sub_datas =
+                SnapshotParallelizer::partition(serial_data, self.parallel_size)?;
+            ParallelizerUtil::sink(serial_sub_datas, sinkers, self.parallel_size, false)
+                .await
+                .unwrap();
         }
-
-        Ok((batch_sub_datas, serial_sub_datas))
+        Ok(())
     }
 }
