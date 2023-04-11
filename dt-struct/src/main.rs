aa950862c0bd2f6b4ba666c0e8424d1d779de4d0
caiqy321 <caiqy87@gmail.com>
2023-04-11 15:22:16 +0800

    feat:dt-struct support table column with serial.& support conflict policy setting

--- dt-struct/src/main.rs
+++ dt-struct/src/main.rs
@@ -15,15 +15,11 @@ mod traits;
 mod utils;
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // if args.len() < 2 {
-    //     panic!("no task_config provided in args");
-    // }
-    // let config = args[1].clone();
-
-    let config = String::from(
-        "/Users/caiqinyu/Desktop/Project/rust/ape-dts/dt-struct/src/test/task_config.ini",
-    );
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("no task_config provided in args");
+    }
+    let config = args[1].clone();
 
     let task_config = TaskConfig::new(&config);
     let struct_config = StructTaskConfig::new(&config);
