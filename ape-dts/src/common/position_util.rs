589ebea069977f0d5fd04cd6b50837ffa2209779
shicai.xu <chenglingsu@126.com>
2023-04-13 16:32:18 +0800

    add padding UTC-000 to positon timestamp

--- ape-dts/src/common/position_util.rs
+++ ape-dts/src/common/position_util.rs
@@ -19,7 +19,7 @@ impl PositionUtil {
         let naive_datetime = NaiveDateTime::from_timestamp_millis(millis);
         naive_datetime
             .unwrap()
-            .format("%Y-%m-%d %H:%M:%S%.3f")
+            .format("%Y-%m-%d %H:%M:%S%.3f UTC-0000")
             .to_string()
     }
 }
@@ -48,12 +48,12 @@ mod test {
     #[test]
     fn test_format_timestamp() {
         assert_eq!(
-            "2023-03-28 07:33:48.396",
+            "2023-03-28 07:33:48.396 UTC-0000",
             PositionUtil::format_timestamp_millis(733304028396543 / 1000 + 946_684_800 * 1000)
         );
 
         assert_eq!(
-            "2023-03-28 05:33:47.000",
+            "2023-03-28 05:33:47.000 UTC-0000",
             PositionUtil::format_timestamp_millis(1679981627 * 1000)
         );
     }
