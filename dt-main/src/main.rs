3219230a065907c1cfba3f15e01888f1f8bec04b
shicai.xu <chenglingsu@126.com>
2024-12-24 23:24:13 +0800

    fix stack backtrace missing when panic happens

--- dt-main/src/main.rs
+++ dt-main/src/main.rs
@@ -1,10 +1,12 @@
-use std::env;
+use std::{env, panic};
 
 use dt_precheck::{config::task_config::PrecheckTaskConfig, do_precheck};
 use dt_task::task_runner::TaskRunner;
 
 #[tokio::main]
 async fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
+
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         panic!("no task_config provided in args");
