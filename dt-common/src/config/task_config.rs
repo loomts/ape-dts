c1d10b9dc5a616133efeaefa1d783cc60950f62b
shicai.xu <chenglingsu@126.com>
2025-02-21 12:07:16 +0800

    enable binlog heartbeat for mysql cdc

--- dt-common/src/config/task_config.rs
+++ dt-common/src/config/task_config.rs
@@ -142,6 +142,11 @@ impl TaskConfig {
                     server_id: loader.get_required(EXTRACTOR, "server_id"),
                     gtid_enabled: loader.get_optional(EXTRACTOR, "gtid_enabled"),
                     gtid_set: loader.get_optional(EXTRACTOR, "gtid_set"),
+                    binlog_heartbeat_interval_secs: loader.get_with_default(
+                        EXTRACTOR,
+                        "binlog_heartbeat_interval_secs",
+                        10,
+                    ),
                     heartbeat_interval_secs,
                     heartbeat_tb,
                     start_time_utc: loader.get_optional(EXTRACTOR, "start_time_utc"),
